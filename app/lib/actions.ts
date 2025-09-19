80eeca64c5ee9634d0e04872c0b0eb409a003f25
esperanza <judithesperanza1983@gmail.com>
2025-09-19 11:48:50 -0500

    se crea la pagina de editar una factura obteniendo su id

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 import postgres from "postgres";
 import { z } from "zod"
 
-const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
     id: z.string(),
@@ -15,10 +15,11 @@ const FormSchema = z.object({
     date: z.string()
 });
 
-const CreateInvoice = FormSchema.omit({ id: true, date: true});
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData){
-    const {customerId, amount, status} =  CreateInvoice.parse({
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
@@ -36,4 +37,23 @@ export async function createInvoice(formData: FormData){
     //Puede hacerlo con la revalidatePathfunción de Next.js
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices'); //dirigir a invoices al crear la factura
+}
+
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+    await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount=${amountInCents}, status=${status}
+            WHERE id = ${id}
+        `;
+    revalidatePath('/dasboard/invoices');
+    redirect('/dashboard/invoices')
+
 }
