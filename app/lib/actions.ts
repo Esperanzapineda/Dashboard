ac6e8b7e037503bf3bd8a4e852f37ab705362e9c
esperanza <judithesperanza1983@gmail.com>
2025-09-24 15:47:45 -0500

    Se realiza la validacion para el formulario de create invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,25 +5,49 @@ import { redirect } from "next/navigation";
 import postgres from "postgres";
 import { z } from "zod"
 
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer'
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoices status.'
+    }),
     date: z.string()
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevSate: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.'
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100; //convertir la cantiad a centavos para eliminar errores de punto flotante
     const date = new Date().toISOString().split('T')[0]; //creemos una nueva fecha con el formato "AAAA-MM-DD" para la fecha de creación de la factura
     try {
@@ -40,7 +64,6 @@ export async function createInvoice(formData: FormData) {
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices'); //dirigir a invoices al crear la factura
 }
-
 
 export async function updateInvoice(id: string, formData: FormData) {
     const { customerId, amount, status } = UpdateInvoice.parse({
